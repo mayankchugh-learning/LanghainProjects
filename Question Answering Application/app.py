bfb02bc454b7f20896a83ee8400e0d26b58d8bbd
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-06-07 10:40:10 +0800

    update app.py

--- Question Answering Application/app.py
+++ Question Answering Application/app.py
@@ -8,25 +8,8 @@ from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM, Conversa
 
 #As Langchain team has been working aggresively on improving the tool, we can see a lot of changes happening every weeek,
 #As a part of it, the below import has been depreciated
-#from langchain.llms import OpenAI
-
-#New import from langchain, which replaces the above
-from langchain_openai import OpenAI
 
 #When deployed on huggingface spaces, this values has to be passed using Variables & Secrets setting, as shown in the video :)
-#import os
-#os.environ["OPENAI_API_KEY"] = "sk----"
-
-#import os
-#from dotenv import load_dotenv
-
-#load_dotenv()
-
-#os.environ["ANY_SCALE_API_KEY"]=os.getenv("ANY_SCALE_API_KEY")
-#os.environ["ANY_SCALE_API_BASE"]=os.getenv("ANY_SCALE_API_BASE")
-#anyscale_base_url = os.environ["ANY_SCALE_API_BASE"]
-#anyscale_api_key = os.environ["ANY_SCALE_API_KEY"]
-#model = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
 
 # response_generator = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
 
@@ -42,16 +25,11 @@ response_generator = pipeline("text-generation", model=modelname, tokenizer=toke
 #Function to return the response
 def load_answer(question):
     answer = response_generator(question)
-    # "text-davinci-003" model is depreciated, so using the latest one https://platform.openai.com/docs/deprecations
-    #llm = OpenAI(model_name=model,temperature=0,base_url=anyscale_base_url,api_key=anyscale_api_key)
-
-    #Last week langchain has recommended to use invoke function for the below please :)
-    #answer=llm.invoke(question)
     return answer
 
 
 #App UI starts here
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.set_page_config(page_title="LangChain Demo - Chatbot ", page_icon=":robot:")
 st.header("LangChain Demo")
 
 #Gets the user input
